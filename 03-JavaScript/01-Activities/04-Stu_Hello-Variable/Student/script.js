7ac238e588b063a5a38d6ac384f13ef47f253195
nguyent0275 <nguyent0275@gmail.com>
2023-09-28 21:39:46 -0400

    added student code

---
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Student/script.js
@@ -0,0 +1,9 @@
+// Write Your JavaScript Code Here
+var personName = "Toan.";
+var pets = 1;
+var funFact = 'I love playing video games.';
+
+
+console.log('My name is ' + personName);
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
