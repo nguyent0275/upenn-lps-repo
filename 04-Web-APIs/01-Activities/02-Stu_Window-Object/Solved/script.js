e3a95afdb48859f9a9f490ba850b01e0eb4d59ce
amaddatu <>
2023-10-10 17:56:50 -0400

    Add solved

---
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Solved/script.js
@@ -0,0 +1,11 @@
+// Logs window object / holds the entire web api
+console.log(window);
+
+// Logs reference to the document in the window object (DOM)
+console.log(window.document);
+
+// Logs root element of document. (html and shows you everything inside)
+console.log(document.documentElement);
+
+// Returns head element of current document (head)
+console.log(document.head);
